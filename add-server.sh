46f3d6a14f295717768c727c852e6c24c95de9a4
Jon Hall <jonhall@us.ibm.com>
2021-04-26 13:59:48 -0500

    Update add-server.sh

--- add-server.sh
+++ add-server.sh
@@ -2,8 +2,7 @@
 # get this servers instanceid from cloud-init
 
 export USERNAME="admin"
-export PASSWORD="imageconversion1234"
-REDIS_CLI="redli -u rediss://$USERNAME:$PASSWORD@25a8ac71-9f05-4ddf-9768-e5546ab67dbb.bsbaodss0vb4fikkn2bg.private.databases.appdomain.cloud:30129/0 --certfile=/root/da4adf1d-5570-4714-b526-f6d3e202e02e"
+REDIS_CLI="redli -u rediss://$USERNAME:$REDIS_PASSWORD@25a8ac71-9f05-4ddf-9768-e5546ab67dbb.bsbaodss0vb4fikkn2bg.private.databases.appdomain.cloud:30129/0 --certfile=/root/da4adf1d-5570-4714-b526-f6d3e202e02e"
 q1="queue"
 
 push="${REDIS_CLI} RPUSH $q1 $1"
