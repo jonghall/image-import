2d4fc670a9242c435ff42c02a0141ad8dc57d514
jonhall <jonhall@us.ibm.com>
2021-04-27 06:54:16 -0500

    Image server createion TF plan

--- add-server.sh
+++ add-server.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 # get this servers instanceid from cloud-init
 
-export USERNAME="admin"
-export PASSWORD="imageconversion1234"
-REDIS_CLI="redli -u rediss://$USERNAME:$PASSWORD@25a8ac71-9f05-4ddf-9768-e5546ab67dbb.bsbaodss0vb4fikkn2bg.private.databases.appdomain.cloud:30129/0 --certfile=/root/da4adf1d-5570-4714-b526-f6d3e202e02e"
+REDIS_CLI="redli -u rediss://$REDISUSER:$REDISPW@25a8ac71-9f05-4ddf-9768-e5546ab67dbb.bsbaodss0vb4fikkn2bg.private.databases.appdomain.cloud:30129/0 --certfile=/root/da4adf1d-5570-4714-b526-f6d3e202e02e"
 q1="queue"
 
 push="${REDIS_CLI} RPUSH $q1 $1"
