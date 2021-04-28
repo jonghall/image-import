00e1766758dad44b0603046144482b16abc86cf4
Jon Hall <jonhall@us.ibm.com>
2021-04-27 21:19:13 -0500

    Update add-server.sh

--- add-server.sh
+++ add-server.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # get this servers instanceid from cloud-init
 
-REDIS_CLI="redli -u rediss://$REDISUSER:$REDISPW@$REDISURL --certfile=/root/da4adf1d-5570-4714-b526-f6d3e202e02e"
+REDIS_CLI="redli -u rediss://$REDISUSER:$REDISPW@$REDISURL --certfile=$REDIS_CACERT"
 q1="queue"
 
 push="${REDIS_CLI} RPUSH $q1 $1"
