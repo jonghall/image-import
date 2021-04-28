a06c423738a8c284396e0426e5810bfeb3d91497
Jon Hall <jonhall@us.ibm.com>
2021-04-27 21:53:14 -0500

    Update create-image-background.sh

--- create-image-background.sh
+++ create-image-background.sh
@@ -1,10 +1,17 @@
 #!/usr/bin/env bash
-# Script assumes setup-image-server.sh has installed all pre-reqs
-# Script assumes /mnt/cos is mounted with COS bucket which is accessible by Image Import in destination region. HMAC keys stored in ~/.passwd-s3fs
-# Script assume IBM Cloud apikey stored as ~/apikey.json
-# command usage:
-# create-image.sh
-# run add-server.sh to add servernames to queue.
+# create-image-background.sh - A script to convert snapshots to custom images
+# Author: Jon Hall
+# Copyright (c) 2021
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#     http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 # Set Variables
 set -e
