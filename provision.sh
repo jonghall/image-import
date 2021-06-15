d42046cfaa064f6820e49f50f2482844285e7fc0
Jon Hall <jonhall@us.ibm.com>
2021-06-15 18:16:29 -0500

    Update provision.sh

--- provision.sh
+++ provision.sh
@@ -3,13 +3,7 @@
 # Script provisions new server from snapshot.
 # Command Usage:
 # provision.sh servername region vpc zone profile_name subnet snapshot
-#    IBMCLOUD CLI
-#    ibmcloud is instance-create INSTANCE_NAME VPC ZONE_NAME PROFILE_NAME SUBNET [--image-id IMAGE_ID] [--boot-volume BOOT_VOLUME_JSON | @BOOT_VOLUME_JSON_FILE] [--volume-attach VOLUME_ATTACH_JSON | @VOLUME_ATTACH_JSON_FILE] [--key-ids IDS] [--dedicated-host HOST_ID | --dedicated-host-group HOST_GROUP_ID] [--user-data DATA] [([--security-group-ids SECURITY_GROUP_IDS] [--ipv4 IPV4_ADDRESS] [--allow-ip-spoofing false | true]) | --primary-network-interface PRIMARY_NETWORK_INTERFACE_JSON | @PRIMARY_NETWORK_INTERFACE_JSON_FILE] [--network-interface NETWORK_INTERFACE_JSON | @NETWORK_INTERFACE_JSON_FILE] [--resource-group-id RESOURCE_GROUP_ID | --resource-group-name RESOURCE_GROUP_NAME] [--output JSON] [-i, --interactive] [-q, --quiet]
-#     INSTANCE_NAME: Name of the instance.
-#     VPC:           ID of the VPC.
-#     ZONE_NAME:     Name of the zone.
-#     PROFILE_NAME:  Name of the profile.
-#     SUBNET:        ID of the subnet.
+
 
 if [ $# -ne 7 ];
     then
@@ -51,6 +45,14 @@ export snapshotid=$(ibmcloud is snapshots --json | jq -r '.[] | select(.name == 
 echo "Snapshot $snapshot ($snapshotid) identified."
 
 echo "Provisioning $servername from snapshot $snapshot."
+
+#    IBMCLOUD CLI
+#    ibmcloud is instance-create INSTANCE_NAME VPC ZONE_NAME PROFILE_NAME SUBNET --boot-volume BOOT_VOLUME_JSON
+#     INSTANCE_NAME: Name of the instance.
+#     VPC:           ID of the VPC.
+#     ZONE_NAME:     Name of the zone.
+#     PROFILE_NAME:  Name of the profile.
+#     SUBNET:        ID of the subnet.
 
 result=$(ibmcloud is instance-create $servername $vpcid $zone $profile_name $subnetid --boot-volume "{\"name\": \"boot-vol-attachment-$servername\", \"volume\": {\"name\": \"boot-vol-name-$servername\", \"profile\": {\"name\": \"general-purpose\"}, \"source_snapshot\": {\"id\": \"$snapshotid\"}}}" --json)
 
